c441ba0a7f0f727ab2ab3d7274734bf894f14486
Powermatrix12 <167418825+Powermatrix12@users.noreply.github.com>
2024-10-11 23:52:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -115,7 +115,7 @@ DISABLE_CHANNEL_BUTTON = True if os.environ.get("DISABLE_CHANNEL_BUTTON", "TRUE"
 
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "6706434927").split()):
+    for x in (os.environ.get("ADMINS", "5904478052").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
