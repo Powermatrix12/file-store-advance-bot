ca4553ea035b2ff9b92067b8eb4b3fa64eba92d6
Powermatrix12 <167418825+Powermatrix12@users.noreply.github.com>
2024-10-23 22:59:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -101,4 +101,4 @@ class Bot(Client):
 
     async def stop(self, *args):
         await super().stop()
-        self.LOGGER(__name__).info("Bot stopped.contact @rohit_1888")
+        self.LOGGER(__name__).info("Bot stopped.contact @POWERMODOWNER")
